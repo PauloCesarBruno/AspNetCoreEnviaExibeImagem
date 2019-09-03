ccc0ca8f9d926315e5789509a96470da447bdab5
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2019-09-03 12:29:48 -0300

    ASP

--- AspCore_EnviaExibeImagem/Startup.cs
+++ AspCore_EnviaExibeImagem/Startup.cs
@@ -18,7 +18,7 @@ namespace AspNetCore_EnviaExibeImagem
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
